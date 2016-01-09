5e947dd8c90b34b3afad1f16fafe51b50d439510
Tom Waters <github@random-works.co.uk>
2016-01-09 22:33:16 +0000

    Added comments

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+// Add a dependency on the 'tomw' module which contains the 
+// dialog directive
 angular.module('app', ['tomw'])
   .controller('DlgDemo', function($scope) {
+    // Hide the dialog initially
     $scope.showPop = false;
     
+    // When the example button is clicked, show a dialog
     $scope.confirmAction = function() {
       $scope.dlgTitle = 'Are you sure?';
       $scope.dlgContent = 'Are you sure you want to confirm this action?';
       $scope.showPop = true;
     };
     
+    // If the OK dialog button is pressed, carry out our ation
     $scope.actionConfirmed = function() {
       console.log('Action was confirmed');
     };
--- dlg.js
+++ dlg.js
@@ -1,20 +1,30 @@
 angular.module('tomw', [])
   .directive('tomwDlg', function() {
     return {
+      // Only apply too elements with attribute tomw-dlg
       restrict: 'A',
+      // Include any child elements in ng-transclude in the template
       transclude: true,
+      // Template for the dialog
       templateUrl: 'dlg.html',
       scope: {
+        // Title for the dialog
         dlgTitle: '=',
+        // Body text for the dialog
         dlgContent: '=',
+        // Bool to show or hide the dialog
         dlgShow: '=',
+        // Function to call when the OK button is clicked
         dlgAction: '&'
       }, 
       controller: function($scope) {
+        // When OK is clicked, close the dialog
+        // and call the supplied dlgAction
         $scope.posClick = function() {
           $scope.dlgShow = false;
           $scope.dlgAction();
         };
+        // When Cancel is clicked, close the dialog
         $scope.negClick = function() {
           $scope.dlgShow = false;
         };
